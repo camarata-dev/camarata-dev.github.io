aaac19b814331c1d674e463c0377152135bd2486
Glitch (hello-webpage) <none>
2020-05-13 18:08:43 +0000

    🏯🚍 Checkpoint ./server.js:9917336/4866

--- server.js
+++ server.js
@@ -2,79 +2,87 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
-
 
 //-------------------------------------------------------------//
 
-
 // init Spotify API wrapper
-var SpotifyWebApi = require('spotify-web-api-node');
+var SpotifyWebApi = require("spotify-web-api-node");
 
 // Replace with your redirect URI, required scopes, and show_dialog preference
 var redirectUri = `https://${process.env.PROJECT_DOMAIN}.glitch.me/cb/ls.html`;
-var scopes = ['user-top-read'];
+var scopes = ["user-top-read"];
 var showDialog = true;
 
 // The API object we'll use to interact with the API
 var spotifyApi = new SpotifyWebApi({
-  clientId : process.env.CLIENT_ID,
-  clientSecret : process.env.CLIENT_SECRET,
-  redirectUri : redirectUri
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: redirectUri
 });
 
-app.get("/authorize", function (request, response) {
+app.get("/authorize", function(request, response) {
   var authorizeURL = spotifyApi.createAuthorizeURL(scopes, null, showDialog);
-  console.log(authorizeURL)
+  console.log(authorizeURL);
   response.send(authorizeURL);
 });
 
 // Exchange Authorization Code for an Access Token
-app.get("/callback", function (request, response) {
+app.get("/callback", function(request, response) {
   var authorizationCode = request.query.code;
-  
-  spotifyApi.authorizationCodeGrant(authorizationCode)
-  .then(function(data) {
-    console.log(data)
-    response.redirect(`/#access_token=${data.body['access_token']}&refresh_token=${data.body['refresh_token']}`)
-  }, function(err) {
-    console.log('Something went wrong when retrieving the access token!', err.message);
-  });
+
+  spotifyApi.authorizationCodeGrant(authorizationCode).then(
+    function(data) {
+      console.log(data);
+      response.redirect(
+        `/#access_token=${data.body["access_token"]}&refresh_token=${
+          data.body["refresh_token"]
+        }`
+      );
+    },
+    function(err) {
+      console.log(
+        "Something went wrong when retrieving the access token!",
+        err.message
+      );
+    }
+  );
 });
 
-app.get("/logout", function (request, response) {
-  response.redirect('/'); 
+app.get("/logout", function(request, response) {
+  response.redirect("/");
 });
 
-app.get('/myendpoint', function (request, response) {
+app.get("/myendpoint", function(request, response) {
   var loggedInSpotifyApi = new SpotifyWebApi();
-  console.log(request.headers['authorization'].split(' ')[1]);
-  loggedInSpotifyApi.setAccessToken(request.headers['authorization'].split(' ')[1]);
+  console.log(request.headers["authorization"].split(" ")[1]);
+  loggedInSpotifyApi.setAccessToken(
+    request.headers["authorization"].split(" ")[1]
+  );
   // Search for a track!
-  loggedInSpotifyApi.getMyTopTracks()
-    .then(function(data) {
+  loggedInSpotifyApi.getMyTopTracks().then(
+    function(data) {
       console.log(data.body);
       response.send(data.body);
-    }, function(err) {
+    },
+    function(err) {
       console.error(err);
-    });
-  
+    }
+  );
 });
-
 
 //-------------------------------------------------------------//
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
